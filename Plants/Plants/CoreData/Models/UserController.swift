ddfbb3af8313ded1f5775eac8f7b2b677c686329
Troy Christian <apostasy8@gmail.com>
2020-02-03 17:01:48 -0500

    add Token struct, write SignUp func in UserController

--- Plants/Plants/CoreData/Models/UserController.swift
+++ Plants/Plants/CoreData/Models/UserController.swift
@@ -29,7 +29,60 @@ class UserController{
     private let baseURL = URL(string: "https://water-my-plants-1.herokuapp.com/api")!
     private let signUpURL = URL(string: "https://water-my-plants-1.herokuapp.com/api/users/register" )!
     
-    private let loginUserURL = URL(string: "https://water-my-plants-1.herokuapp.com/api/users/register/users/login")
+    private let loginUserURL = URL(string: "https://water-my-plants-1.herokuapp.com/api/users/register/users/login")!
+    
+    var authToken:Token?
+    
+    
+    func signUp(with user: UserRepresentation, completion: @escaping (Error?) -> ()) {
+       
+        
+        var request = URLRequest(url: signUpURL)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let jsonEncoder = JSONEncoder()
+        do {
+            let jsonData = try jsonEncoder.encode(user)
+            request.httpBody = jsonData
+        } catch {
+            print("Error encoding user object: \(error)")
+            completion(error)
+            return
+        }
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                completion(NSError(domain: "", code: response.statusCode, userInfo:nil))
+                return
+            }
+            
+            if let error = error {
+                completion(error)
+                return
+            }
+            guard let data = data else {
+                completion(NSError())
+                return
+            }
+            
+                    let decoder = JSONDecoder()
+                   // decoder.keyDecodingStrategy = .convertFromSnakeCase
+                    do {
+                        self.authToken = try decoder.decode(Token.self, from: data)
+                        print("Success signing up  your role_id is: \(String(describing: self.authToken?.token))")
+                        
+                    } catch {
+                        print("Error decoding id object: \(error)")
+                        completion(error)
+                        return
+                    }
+                    
+                    completion(nil)
+                }.resume()
+            
+            
+    }
     
     
 }
